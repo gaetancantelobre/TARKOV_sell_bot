85b604d59e4a1edb82bee5837e007a56949c40f8
TheLoneWolf02 <gaetancantelobre@gmail.com>
2023-08-31 21:17:35 +0200

    ADDED IMAGES OF TABS

---
+++ tarkov_navigation.py
@@ -0,0 +1,13 @@
+import os
+import cv2 as cv
+
+
+MAIN_MENU = 0
+INVENTORY = 1
+TRADERS = 2
+FLEA_MARKET = 3
+
+def identify_current_menu():
+    scanned_image = cv.imread(screen_shot, cv.IMREAD_UNCHANGED)
+        fir_image = cv.imread(fir_type, cv.IMREAD_UNCHANGED)
+    
